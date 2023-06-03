6654337c3ecbf4336ebede05b974bb62a8953ad1
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-06-03 13:32:57 +0200

    Initial Commit

---
+++ NetworkCall/NetworkCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkCallApp.swift
+//  NetworkCall
+//
+//  Created by Eric on 03/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
